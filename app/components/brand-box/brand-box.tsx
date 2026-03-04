47ef82979ac96f88914117029de627af6b6261f5
Leemainsw <yebom6476@gmail.com>
2026-03-04 16:53:38 +0900

    feat: 브랜드 박스 컴포넌트 추가 (#21)

---
+++ app/components/brand-box/brand-box.tsx
@@ -0,0 +1,51 @@
+import { subscriptableBrand } from "@/app/utils/brand/brand";
+import { SubscriptableBrandType } from "@/app/utils/brand/type";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+type Size = "sm" | "lg";
+
+interface Props {
+  brandType: SubscriptableBrandType;
+  size: Size;
+  className?: string;
+  isActive?: boolean;
+  onClick?: () => void;
+}
+
+const sizeMap = {
+  sm: "w-12 h-12 rounded-xl",
+  lg: "w-20 h-20 rounded-[20px]",
+};
+
+const imageSizeMap = {
+  sm: 40,
+  lg: 66,
+};
+
+export default function BrandBox({
+  brandType,
+  size,
+  className,
+  onClick,
+  isActive,
+}: Props) {
+  return (
+    <div
+      className={cn(
+        `flex items-center justify-center border border-gray-100`,
+        onClick && `cursor-pointer`,
+        onClick && isActive && `bg-blue-50 border-blue-300`,
+        sizeMap[size],
+        className
+      )}
+    >
+      <Image
+        src={subscriptableBrand[brandType].image}
+        alt={subscriptableBrand[brandType].label}
+        width={imageSizeMap[size]}
+        height={imageSizeMap[size]}
+      />
+    </div>
+  );
+}
