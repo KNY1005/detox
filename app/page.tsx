8b6ecfbb239c2282232f62634440b5a1a87f5f6d
lyla <bgy09270@naver.com>
2026-03-04 21:55:30 +0900

    refactor: 메인페이지 빈페이지로 초기화 (#22)

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-import BottomNav from "./components/bottomNav";
-import FloatingButton from "./components/floatingButton";
-
 export default function Home() {
-  return (
-    <>
-      <BottomNav />
-      <div className="btn-wrap fixed bottom-21 right-0 flex flex-col gap-2">
-        <FloatingButton variant="create" />
-        <FloatingButton variant="top" />
-      </div>
-    </>
-  );
+  return <>main</>;
 }
